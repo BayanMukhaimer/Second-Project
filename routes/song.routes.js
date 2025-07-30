328749a722054bfec53415669617175b78fb9765
Bayan <bayanessam40@gmail.com>
2025-07-30 11:54:19 +0300

    updating the playlist route

--- routes/song.routes.js
+++ routes/song.routes.js
@@ -18,5 +18,54 @@ router.post("/", async (req, res) => {
 });
 
 
+router.get("/", async (req, res) => {
+  try {
+    const allSongs = await Song.find();
+    res.render("songs/all-songs.ejs", { allSongs });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+router.get("/:songId", async (req, res) => {
+  try {
+    const foundSong = await Song.findById(req.params.songId);
+    res.render("songs/song-details.ejs", { foundSong });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
+router.get("/:songId/edit", async (req, res) => {
+  try {
+    const song = await Song.findById(req.params.songId);
+    res.render("songs/song-edit.ejs", { song });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+router.put("/:songId", async (req, res) => {
+  try {
+    await Song.findByIdAndUpdate(req.params.songId, req.body);
+    res.redirect("/songs");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+router.delete("/:songId", async (req, res) => {
+  try {
+    await Song.findByIdAndDelete(req.params.songId);
+    res.redirect("/songs");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
