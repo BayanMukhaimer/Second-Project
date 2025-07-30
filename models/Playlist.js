3e529b05ebd324e61ec46abdbf0e908192d8450b
Bayan <bayanessam40@gmail.com>
2025-07-30 20:30:27 +0300

    updating playlist route

--- models/Playlist.js
+++ models/Playlist.js
@@ -8,7 +8,7 @@ const playlistSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    // required: true
   },
   songIds: [{
     type: mongoose.Schema.Types.ObjectId,
--- routes/playlist.route.js
+++ routes/playlist.route.js
@@ -19,6 +19,7 @@ router.get("/new", async (req, res) => {
 router.post("/",async(req,res)=> {
     try{
         const createdPlaylist = await Playlist.create(req.body)
+        console.log(allSongs)
         res.redirect("/playlists/new")
     }
     catch(error){
@@ -48,6 +49,36 @@ router.get("/:playlistId", async (req, res) => {
 
 
 
+router.get("/:playlistId/edit", async (req, res) => {
+  try {
+    const foundPlaylist = await Playlist.findById(req.params.playlistId);
+    const allSongs = await Song.find();
+    res.render("playlists/playlist-edit.ejs", { foundPlaylist, allSongs });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
+router.put("/:playlistId", async (req, res) => {
+  try {
+    await Playlist.findByIdAndUpdate(req.params.playlistId, req.body);
+    res.redirect(`/playlists/${req.params.playlistId}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+router.delete("/:playlistId", async (req, res) => {
+  try {
+    await Playlist.findByIdAndDelete(req.params.playlistId);
+    res.redirect("/playlists");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 
 module.exports = router
